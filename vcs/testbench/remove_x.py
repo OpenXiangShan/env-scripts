5d1d68f5a629b2159ceb40d0f85612d7af481510
Yinan Xu <xuyinan@ict.ac.cn>
2021-05-31 13:20:53 +0800

    add python script to force x to be random

--- vcs/testbench/remove_x.py
+++ vcs/testbench/remove_x.py
@@ -7,7 +7,6 @@ need_initial = [
   ("`CORE.memBlock.dcache.storeReplayUnit.resp_arb.lastGrant", 4),
   ("`CORE.memBlock.dcache.probeQueue.pipe_req_arb.lastGrant", 4),
   ("`CORE.memBlock.dcache.mainPipeReqArb.lastGrant", 2),
-  ("`CORE.memBlock.dcache.missReqArb.lastGrant", 2),
 ]
 for i in range(256):
   need_initial.append((f"`CORE.l1pluscache.pipe.REG_1_{i}", 7))
@@ -24,14 +23,16 @@ need_force = [
   # unknown reason (fetch x?)
   ("`CORE.frontend.instrUncache.io_resp_bits_data", 256),
   ("`CORE.frontend.instrUncache.entries_0.io_resp_bits_data", 256),
-  ("`CORE.frontend.ifu.icache.pds_0.io_in_data", 256),
-  ("`CORE.frontend.ifu.icache.pds_1.io_in_data", 256),
-  ("`CORE.frontend.ifu.icache.pds_2.io_in_data", 256),
-  ("`CORE.frontend.ifu.icache.pds_3.io_in_data", 256),
-  ("`CORE.frontend.ifu.icache.pds_0.io_in_mask", 16),
-  ("`CORE.frontend.ifu.icache.pds_1.io_in_mask", 16),
-  ("`CORE.frontend.ifu.icache.pds_2.io_in_mask", 16),
-  ("`CORE.frontend.ifu.icache.pds_3.io_in_mask", 16),
+  ("`CORE.frontend.ifu.io_redirect_bits_cfiUpdate_pc", 39),#X cause LOOP to be X
+  ("`CORE.frontend.ifu.io_redirect_bits_cfiUpdate_target", 39),#X cause LOOP to be X
+  #("`CORE.frontend.ifu.icache.pds_0.io_in_data", 256),
+  #("`CORE.frontend.ifu.icache.pds_1.io_in_data", 256),
+  #("`CORE.frontend.ifu.icache.pds_2.io_in_data", 256),
+  #("`CORE.frontend.ifu.icache.pds_3.io_in_data", 256),
+  #("`CORE.frontend.ifu.icache.pds_0.io_in_mask", 16),
+  #("`CORE.frontend.ifu.icache.pds_1.io_in_mask", 16),
+  #("`CORE.frontend.ifu.icache.pds_2.io_in_mask", 16),
+  #("`CORE.frontend.ifu.icache.pds_3.io_in_mask", 16),
   # dual-port SRAMs read and write the same index at the same clock cycle
   ("`CORE.frontend.ifu.bpu.bim.bim.array.array_2_ext.R0_data", 32),
   ("`CORE.frontend.ifu.bpu.preds_3.tables_5.lo_us.array.array_3_ext.R0_data", 16),
@@ -61,24 +62,100 @@ need_force = [
   ("`CORE.ctrlBlock.ftq.ftq_2r_sram.SRAMTemplate_1.array.array_19_ext.R0_data", 275),
   ("`CORE.ctrlBlock.ftq.ftq_2r_sram.SRAMTemplate.array.array_19_ext.R0_data", 275),
   ("`CORE.ctrlBlock.ftq.pred_target_sram.SRAMTemplate.array.array_20_ext.R0_data", 39),
-  ("`CORE.ctrlBlock.ftq.ftq_1r_sram.SRAMTemplate.array.array_21_ext.R0_data", 944),
+  #("`CORE.ctrlBlock.ftq.ftq_1r_sram.SRAMTemplate.array.array_21_ext.R0_data", 944),
+  ("`CORE.ctrlBlock.rename.FreeList_1.io_req_canAlloc", 1),
+  ("tb_top.sim.CPU.axi4deint.REG_1", 5),
 ]
 
-def generate():
-  for source, width in need_initial:
-    print("initial begin")
-    print(f"force {source} = $random();")
-    print(f"#10 release {source};")
-    print("end")
+need_force_1 = [
+   ("`CORE.ctrlBlock.ftq.ftq_1r_sram.SRAMTemplate.array.array_21_ext.R0_data", 944),
+   ("`CORE.frontend.ifu.icache.icacheMissQueue.io_resp_bits_data", 512),
+]
 
-  for source, width in need_force:
-    print("always @(clock) begin")
+# QN
+all_modules = [
+  ("`CORE.l1pluscache.pipe.", "/home/xyn/debug/gate/vcs_newgate/20210530-gate/XSCore/L1plusCachePipe.v", "REG_1_"),
+  ("`CORE.frontend.ifu.icache.", "/home/xyn/debug/gate/vcs_newgate/20210530-gate/XSCore/ICache.v", "REG_1_"),
+  ("`CORE.memBlock.dcache.mainPipe.", "/home/xyn/debug/gate/vcs_newgate/20210530-gate/XSCore/DCache_MainPipe_0.v", "REG_4_"),
+  ("`CORE.ptw.", "/home/xyn/debug/gate/vcs_newgate/20210530-gate/XSCore/PTW.v", "REG_19_"),
+  ("`CORE.ptw.", "/home/xyn/debug/gate/vcs_newgate/20210530-gate/XSCore/PTW.v", "REG_38_"),
+]
+
+def find_qn(level, filename, prefix):
+  all_qn = []
+  last_line = ""
+  with open(filename) as f:
+    for line in f:
+      if ".QN(" in line:
+        cell_name = last_line.split()[1]
+        if cell_name.startswith(prefix):
+          all_qn.append(level + cell_name)
+        # if not cell_name.startswith("REG_"):
+        #   all_remove = [" l3v", " l2v", " l3_", " l3g", " l1v", " l2_", "ppn_", " l1_", " l1g_", " sp_"]
+        #   found = False
+        #   for x in all_remove:
+        #     if x in last_line or x in line:
+        #       found = True
+        #       continue
+        #   if not found:
+        #     print(last_line.strip() + line)
+      else:
+        last_line = line
+  return all_qn
+
+need_qn = []
+for level, module, prefix in all_modules:
+    need_qn += find_qn(level, module, prefix)
+
+def generate():
+  '''for source, width in need_initial:
+    # print("initial begin")
     for i in range(width):
-      print(f"if ({source}[{i}] === 1'bx) begin")
-      print(f"  force {source}[{i}] = $random();")
+      source_name = f"{source}_reg_{i}_.Q"
+      print("initial begin")
+      print(f"  force {source_name} = $random();")
+      print(f"  force {source_name}N = $random();")
+      print(f"  #10 release {source_name};")
+      print("end")
+'''
+
+  print("always @(clock) begin")
+  for source, width in need_force_1:
+    for i in range(width):
+      source_name = f"{source}_{i}_"
+      if "io_resp_bits_data" in source and i in [328, 135, 79]:
+        source_name += "_BAR"
+      print(f"if ({source_name} === 1'bx) begin")
+      print(f"  force {source_name} = $random();")
       print(f"end")
-      print(f"else begin release {source}[{i}]; end")
-    print("end")
+      print(f"else begin release {source_name}; end")
+
+  for source, width in need_initial:
+    for i in range(width):
+      source_name = f"{source}_reg_{i}_.Q"
+      print(f"if ({source_name} === 1'bx) begin")
+      print(f"  force {source_name} = $random();")
+      print(f"end")
+      print(f"else begin release {source_name}; end")
+
+  for source in need_qn:
+      source_name = f"{source}.QN"
+      print(f"if ({source_name} === 1'bx) begin")
+      print(f"  force {source_name} = $random();")
+      print(f"end")
+      print(f"else begin release {source_name}; end")
+ 
+  for source, width in need_force:
+    for i in range(width):
+      source_name = f"{source}"
+      if width > 1:
+        source_name += f"[{i}]"
+      print(f"if ({source_name} === 1'bx) begin")
+      print(f"  force {source_name} = $random();")
+      print(f"end")
+      print(f"else begin release {source_name}; end")
+  print("end")
 
 if __name__ == "__main__":
   generate()
+
