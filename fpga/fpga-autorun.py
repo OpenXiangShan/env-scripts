313065753a5838ca3e7f952a057f363af9a2c27c
ZhangZifei <1773908404@qq.com>
2022-04-01 13:32:02 +0800

    fpga script: fix fpga_autorun.py's wait_fpga_finish grep logic

--- fpga/fpga-autorun.py
+++ fpga/fpga-autorun.py
@@ -14,25 +14,25 @@ xs_path = sys.argv[1] # xiangshan edition
 result_path = sys.argv[2] # result log
 
 def wait_fpga_finish():
-  while(int(os.popen(f"grep -c END {result_path}").read()) <= current_pos):
+  while(int(os.popen(f"grep -c '======== END ' {result_path}").read()) < current_pos):
     time.sleep(60)
   print("get cmd " + str(current_pos) + " result, run next ('0-0')")
 
 def spec2fpga():
   vivado_cmd = "vivado -mode batch -source /nfs/home/share/fpga/0210xsmini/tcl/onboard-ai1.tcl -tclargs " + xs_path + " "
   for x in spec_todo_list:
-    fpga_job_list.append(vivado_cmd + "/nfs/home/share/fpga/xsbins/" + x.strip() + "/data.txt")
+    fpga_job_list.append(vivado_cmd + "/nfs/home/share/fpga/xsbins50m-md5/" + x.strip() + "/data.txt")
 
 spec2fpga()
-print(fpga_job_list)
+# print(fpga_job_list)
 
 # exit()
 
 while(current_pos < len(fpga_job_list)):
+  wait_fpga_finish()
   cmd = fpga_job_list[current_pos]
   os.system(f"echo run command {current_pos}: " + cmd)
   os.system("date")
   os.system(cmd)
-  wait_fpga_finish()
   current_pos = current_pos + 1
 print("done")
