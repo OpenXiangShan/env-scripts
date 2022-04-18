538788c99f78318c9994fe2fb2d448023b97c956
ZhangZifei <1773908404@qq.com>
2022-04-18 18:36:04 +0800

    fpga script: extract_spec.py now output csv-like result

--- fpga/extract_spec.py
+++ fpga/extract_spec.py
@@ -6,7 +6,7 @@ import re
 
 begin_pat = re.compile(r'======== BEGIN (?P<spec_name>[\w.-]+) ========')
 end_pat   = re.compile(r'===== Finish running SPEC2006 =====')
-time_pat  = re.compile(r'\w+, \d+ \w+ \d+ \d+:\d+:\d+ \+0000')
+time_pat  = re.compile(r'\w+, \d+ \w+ \d+ (?P<time>\d+:\d+:\d+) \+0000')
 log_path = sys.argv[1]
 sync_to_file = (len(sys.argv) == 3)
 output_dir = sys.argv[2] if sync_to_file else "error"
@@ -15,13 +15,22 @@ inside = False
 count = 0
 fail = False
 
+toShell = False
+
 def turnpink(str):
-  return "\033[1;35;40m"+str+"\033[0m"
+  if not toShell:
+    return str
+  else:
+    return "\033[1;35;40m"+str+"\033[0m"
 
 def turnred(str):
-  return "\033[1;31;40m"+str+"\033[0m"
+  if not toShell:
+    return str
+  else:
+    return "\033[1;31;40m"+str+"\033[0m"
 
 with open(log_path) as log:
+  spec_record = ""
   for line in log:
     begin_match = begin_pat.match(line)
     end_match = end_pat.match(line)
@@ -33,7 +42,7 @@ with open(log_path) as log:
       fail = False
       spec_name = begin_match.group("spec_name")
       count = count + 1
-      print(f"Find spec {turnpink(spec_name)}:")
+      spec_record = f"{turnpink(spec_name)}"
       if sync_to_file:
         output_file = open(output_dir + "/" + spec_name + ".log", "w")
         output_file.write(line)
@@ -42,11 +51,12 @@ with open(log_path) as log:
         print(f"error, out but not inside {spec_name}")
         exit()
       inside = False
+      print(spec_record)
       if sync_to_file:
         output_file.write(line)
         output_file.close()
     else:
-      if ("unhandled signal" in line) or ("Segmentation fault" in line):
+      if ("unhandled signal" in line) or ("Segmentation fault" in line) or ("Aborted" in line):
         if (not fail):
           fail = True
           print(f"{turnpink(spec_name)} {turnred('failed')}, please check the log for:")
@@ -56,8 +66,10 @@ with open(log_path) as log:
           output_file.write(line)
         time_match = time_pat.match(line)
         if time_match:
-          print(line, end="")
+          spec_record  += ","+time_match.group("time")
 
-print(f"Executed {count} spec")
-if inside:
-  print(f"Warning: The last spec {spec_name} doesn't finish")
+if toShell:
+  if inside:
+    print(f"\n{count-1} spec finished\nun-finished spec: {turnpink(spec_name)}")
+  else:
+    print(f"{count} spec finished")
