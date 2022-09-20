8f161b25ea33339b9f34d2feddcb46993bf1af6a
ZhangZifei <1773908404@qq.com>
2022-09-21 00:36:27 +0800

    fpga: add a series of fpga auto run scripts
    run_multi_fpga_task.sh: run several batch of fpga task
    fpga-autorun-v2.py: autorun, usage at run_multi_fpga_task.sh
    send_email.py: send email, need email.yml(need manually add)
    uart2cap.py: keep capturing fpga output(tty)
    stop_uart.py: kill uart2cap.py
    stop_vivado.py: kill vivado

---
+++ fpga/uart2cap.py
@@ -0,0 +1,29 @@
+
+import sys
+import serial
+
+class SERIAL_TYPE(object):
+  def __init__(self, ip, port, baudrate, output):
+    self.ip = ip
+    self.serial = serial.Serial(
+      port = port,
+      baudrate = baudrate,
+      parity=serial.PARITY_ODD, # 校验位
+      stopbits=serial.STOPBITS_TWO, # 停止位
+      bytesize=serial.SEVENBITS # 数据位
+    )
+    self.output = output
+
+  def read_serial(self):
+    file = open(self.output, 'a')
+    file.close()
+    while True:
+      data = self.serial.readline()
+      file = open(self.output, 'a')
+      file.writelines(data.decode('utf-8'))
+      file.close()
+
+
+
+ser = SERIAL_TYPE(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
+ser.read_serial()
