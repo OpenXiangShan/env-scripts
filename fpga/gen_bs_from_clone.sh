a8c77ace866054305b5f080c1db4a776b0a94fd1
Ziyue Zhang <zhangziyue21@mails.ucas.edu.cn>
2022-10-11 16:07:46 +0800

    foga: support choose patch

--- fpga/gen_bs_from_clone.sh
+++ fpga/gen_bs_from_clone.sh
@@ -1,16 +1,30 @@
 #! /usr/bin/env zsh
-# Usage: zsh/bash gen_xs_bitstream.sh whichbranch whichdir(abs path)
+# Usage: zsh/bash gen_xs_bitstream.sh whichbranch whichdir(abs path) whichpatch
 set -v
 
 fpga_dir=/nfs/home/share/fpga/fpga-gen
+
+if [ $# -ne 3 ]; then
+    echo "Usage: $0 whichbranch whichdir(abs path) whichpatch"
+    echo "patch path : $fpga_dir/patch"
+    echo "$(ls $fpga_dir/patch) : patch"
+    exit 1
+fi
 
 xsBranch=$1
 bsTag=$2
 bsDir=$fpga_dir/bitgen-$bsTag
 xsDir=$fpga_dir/xs-$bsTag
+xsPatch=$3
+    
 
 echo "generating verilog..."
-zsh gen_xiangshan.sh $xsBranch $xsDir
+zsh gen_xiangshan.sh $xsBranch $xsDir $xsPatch
+
+if [ $? -ne 0 ]; then
+    echo "gen_xiangshan.sh failed"
+    exit 1
+fi
 
 echo "generating bitstream..."
 zsh gen_bitstream.sh $bsDir $xsDir/build
--- fpga/gen_xiangshan.sh
+++ fpga/gen_xiangshan.sh
@@ -1,16 +1,33 @@
 #! /usr/bin/env zsh
-# Usage: zsh/bash gen_xs_bitstream.sh whichbranch whichdir(abs path)
+# Usage: zsh/bash gen_xs_bitstream.sh whichbranch whichdir(abs path) whichpatch
 set -v
+
+fpga_dir=/nfs/home/share/fpga/fpga-gen
+if [ $# -ne 3 ]; then
+    echo "Usage: $0 whichbranch whichdir(abs path) whichpatch"
+    echo "$(ls $fpga_dir/patch) : patch"
+    exit 1
+fi
 
 xsBranch=$1
 xsDir=$2
+xsPatch=$3
 
 git clone https://github.com/OpenXiangShan/XiangShan.git $xsDir
 
 cd $xsDir
 git checkout $xsBranch
 make init
-git apply /nfs/home/share/zzf/fpga.patch
+
+if [ $xsPatch != "0" ]; then
+    if [ -f $fpga_dir/patch/$xsPatch ]; then
+        git apply $fpga_dir/patch/$xsPatch
+    else
+        echo "patch file not found"
+        exit 1
+    fi
+fi
+
 export NOOP_HOME=$(pwd)
 export NEMU_HOME=$(pwd)
 make verilog -j17
