18e58d2a75d139f27023d14d1cf7376d56f6107e
lixianfei <lixianfei@bosc.ac.cn>
2023-07-24 11:32:57 +0800

    fix the file path issue
    Signed-off-by: lixianfei <lixianfei@bosc.ac.cn>

--- xs_nanhu_fpga/src/tcl/global_includes.tcl
+++ xs_nanhu_fpga/src/tcl/global_includes.tcl
@@ -1,4 +1,4 @@
-set file "$ip_dir/soc/syscfg/syscfg_define.v"
+set file "${rtl_dir}/syscfg_define.v"
 set file [file normalize $file]
 set file_obj [get_files -of_objects [get_filesets sources_1] [list "*$file"]]
 set_property -name "is_global_include" -value "1" -objects $file_obj
