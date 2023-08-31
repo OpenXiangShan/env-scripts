e22f58f7823182bf3d7cff6fb3318529328fb164
wangkaifan <wangkaifan@ict.ac.cn>
2023-08-31 17:43:16 +0800

    visual: enhance db plot script, supporting more arguments

--- perf/trace_visualization/db_plot.py
+++ perf/trace_visualization/db_plot.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python3
 
 import os
+import sys
 import matplotlib.pyplot as plt
 import argparse
 import sqlite3
@@ -16,12 +17,15 @@ class DBTrace(object):
         self.addr_col = None
         self.stamp_col = None
         self.emphasis_col = None
+        self.emphasis_value = 1
         self.trace_y = []
         self.trace_y_e = []
         self.trace_x = []
         self.trace_x_e = []
+        self.emphasis_trace = []
         self.cnt = 1
-        self.limit = -1
+        self.start = 1
+        self.finish = sys.maxsize
 
     def set_address(self, addr_col):
         self.addr_col = addr_col
@@ -32,20 +36,34 @@ class DBTrace(object):
     def set_emphasis(self, emphasis_col):
         self.emphasis_col = emphasis_col
 
-    def set_limit(self, limit):
-        self.limit = limit
+    def set_start(self, s):
+        self.start = s
+
+    def set_finish(self, f):
+        self.finish = f
+
+    def reverse_emphasis(self):
+        self.emphasis_value = 1 - self.emphasis_value
 
     def release(self):
         self.conn.close()
 
     def derive_trace(self, debug=False):
-        addr_query = self.conn.cursor().execute('SELECT {} FROM {} LIMIT {}'.format(self.addr_col, self.table, self.limit))
+        addr_sql = 'SELECT {} FROM {} WHERE ID BETWEEN {} AND {}'.format(
+            self.addr_col, self.table, self.start, self.finish)
+        stamp_sql = 'SELECT {} FROM {} WHERE ID BETWEEN {} AND {}'.format(
+            self.stamp_col, self.table, self.start, self.finish)
+        emphasis_sql = 'SELECT {} FROM {} WHERE ID BETWEEN {} AND {}'.format(
+            self.emphasis_col, self.table, self.start, self.finish)
+
+        addr_query = self.conn.cursor().execute(addr_sql)
         stamp_query = self.conn.cursor()
         emphasis_query = self.conn.cursor()
+
         if self.stamp_col is not None:
-            stamp_query = self.conn.cursor().execute('SELECT {} FROM {} LIMIT {}'.format(self.stamp_col, self.table, self.limit))
+            stamp_query = self.conn.cursor().execute(stamp_sql)
         if self.emphasis_col is not None:
-            emphasis_query = self.conn.cursor().execute('SELECT {} FROM {} LIMIT {}'.format(self.emphasis_col, self.table, self.limit))
+            emphasis_query = self.conn.cursor().execute(emphasis_sql)
         for addr_q in addr_query:
             addr = addr_q[0]
             if self.emphasis_col is None:
@@ -55,13 +73,15 @@ class DBTrace(object):
                 else:
                     self.trace_x.append(stamp_query.fetchone()[0])
             else:
-                if emphasis_query.fetchone()[0] == 1:
+                if emphasis_query.fetchone()[0] == self.emphasis_value:
+                    self.emphasis_trace.append(1)
                     self.trace_y_e.append(addr)
                     if self.stamp_col is None:
                         self.trace_x_e.append(self.cnt)
                     else:
                         self.trace_x_e.append(stamp_query.fetchone()[0])
                 else:
+                    self.emphasis_trace.append(0)
                     self.trace_y.append(addr)
                     if self.stamp_col is None:
                         self.trace_x.append(self.cnt)
@@ -78,10 +98,26 @@ class DBTrace(object):
                 for i in range(100):
                     print(self.trace_y_e[i])
 
-    def plot(self, r):
+    def emphasis_rate_rolling(self, aggregate=1):
+        aggcnt = 0
+        aggydata = 0
+        ydata = []
+        for p in self.emphasis_trace:
+            aggcnt += 1
+            aggydata += p
+            if aggcnt == aggregate:
+                ydata.append(aggydata / aggregate)
+                aggcnt = 0
+                aggydata = 0
+        xdata = range(len(ydata))
+        plt.plot(xdata, ydata, c='#000000', alpha=1, lw=1, ls='-')
+        plt.show()
+
+    def plot(self, r, emphasis_only):
         print("Painting...")
-        plt.scatter(self.trace_x, self.trace_y, c='#000000',alpha=1, s=50)
-        plt.scatter(self.trace_x_e, self.trace_y_e, c='#FF0000', alpha=1, s=50)
+        if emphasis_only is None:
+            plt.scatter(list(map(lambda x:x-self.trace_x[0], self.trace_x)), self.trace_y, c='#000000',alpha=1, s=1)
+        plt.scatter(list(map(lambda x:x-self.trace_x_e[0], self.trace_x_e)), self.trace_y_e, c='#FF0000', alpha=1, s=1)
         plt.show()
 
 
@@ -92,7 +128,12 @@ if __name__ == "__main__":
     parser.add_argument('-a', '--address', action='store', type=str, help='Address column of database')
     parser.add_argument('-t', '--time-stamp', action='store', type=str, help='Time stamp column of database')
     parser.add_argument('-e', '--emphasis', action='store', type=str, help='Emphasis flag column of database')
-    parser.add_argument('-l', '--limit', action='store', type=str, help='Limit of datapoints')
+    parser.add_argument('-r', '--reverse', action='store_true', default=None, help='Reverse emphasis')
+    parser.add_argument('-s', '--start', action='store', type=int, help='Start limit of datapoints')
+    parser.add_argument('-f', '--finish', action='store', type=int, help='Finish limit of datapoints')
+    parser.add_argument('--emphasis-only', action='store_true', default=None, help='Show emphasis only')
+    parser.add_argument('--emphasis-rate-rolling', action='store_true', default=None, help='Show emphasis rate rolling')
+    parser.add_argument('-g', '--rolling-aggregation', action='store', type=int, help='Rolling aggregation')
     args = parser.parse_args()
 
     if args.address is None:
@@ -104,8 +145,14 @@ if __name__ == "__main__":
     trace.set_time_stamp(args.time_stamp)
     if args.emphasis is not None:
         trace.set_emphasis(args.emphasis)
-    if args.limit is not None:
-        trace.set_limit(args.limit)
+    if args.start is not None:
+        trace.set_start(args.start)
+    if args.finish is not None:
+        trace.set_finish(args.finish)
+    if args.reverse is not None:
+        trace.reverse_emphasis()
     trace.derive_trace()
-    trace.plot(-1)
+    trace.plot(-1, args.emphasis_only)
+    if args.emphasis_rate_rolling is not None:
+        trace.emphasis_rate_rolling(1 if args.rolling_aggregation is None else args.rolling_aggregation)
     trace.release()
