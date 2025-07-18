8c684a1c547fe46c9584d8e1a6935511d57ec500
fengkehan <fengkehan@bosc.ac.cn>
2025-07-18 17:57:53 +0800

    fpga: Add the burn script and test bin

---
+++ xs_kmh_fpga_diff/tools/jtag_write_ddr.sh
@@ -0,0 +1,5 @@
+vivado -mode tcl -source jtag_write_ddr.tcl -tclargs /nfs/home/fengkehan/project/vivado_dma_send/microbench-xs-no-uart.txt
+#vivado -mode tcl -source jtag_write_ddr.tcl -tclargs /nfs/home/fengkehan/project/vivado_dma_send/hello-xs.txt
+
+rm ./*.jou
+rm ./*.log
