ea0ddecc92c50b93ed279793207df27dcf04350b
ZhangZifei <zhangzifei16@mails.ucas.ac.cn>
2025-09-04 10:21:14 +0800

    feat(fpga): init xs_kmh_fpga_tracertl, copy from *_diff

---
+++ xs_kmh_fpga_tracertl/src/tcl/ip_files.tcl
@@ -0,0 +1,8 @@
+set ip_files [list \
+ [file normalize "${rtl_dir}/XSTop_wrapper.sv" ]\
+ [file normalize "${rtl_dir}/mode_ctrl.v"] \
+ [file normalize "${rtl_dir}/syscfg.v"] \
+ [file normalize "${rtl_dir}/syscfg_define.v"] \
+ [file normalize "${rtl_dir}/sys_define.vh"] \
+ [file normalize "${rtl_dir}/RST_SYNC.v"] \
+]
