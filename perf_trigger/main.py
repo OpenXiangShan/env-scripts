9c917efafb38e927a30c9387746f300ab009b0a7
Muzi <lyj_muzi@qq.com>
2026-02-16 09:15:43 +0800

    feat: do not use open servers unless explicitly specified

--- perf_trigger/main.py
+++ perf_trigger/main.py
@@ -155,11 +155,13 @@ class XiangShan:
         nemu_so_path: str | None,
         server_list: str,
     ) -> None:
+        # Do not use open servers unless explicitly specified, as they are too slow
         if server_list == "all":
-            server_pool = SERVER_POOL
+            server_pool = list(filter(lambda s: s.startswith("node"), SERVER_POOL))
         elif server_list == "":
-            desired_sever_num = min(len(self.checkpoints) // 64 + 1, len(SERVER_POOL))
-            server_pool = random.sample(SERVER_POOL, k=desired_sever_num)
+            serer_pool = list(filter(lambda s: s.startswith("node"), SERVER_POOL))
+            desired_sever_num = min(len(self.checkpoints) // 64 + 1, len(serer_pool))
+            server_pool = random.sample(serer_pool, k=desired_sever_num)
         else:
             server_pool = server_list.replace(" ", "").split(",")
             for server in server_pool:
